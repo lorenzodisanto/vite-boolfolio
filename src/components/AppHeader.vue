43c7b098370824d8b6b2414bb2b487dc851c3d44
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-14 17:18:45 +0200

    feat: add header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  // ricevo titolo dall'App via props
+  props: { title: String },
+};
+</script>
+
+<template>
+  <header>
+    <nav class="navbar text-bg-dark">
+      <div class="container">
+        <a class="navbar-brand text-white fs-3" href="#"> {{ title }} </a>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  position: sticky;
+  top: 0;
+}
+</style>
