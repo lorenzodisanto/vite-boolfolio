0638a565921e29e415ef148cc8dece11de4b832a
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-14 15:25:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Boolfolio Vue + Vite",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
