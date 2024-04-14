9aee8a11c03dea898ab3db17a78057abec3e9648
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-14 17:18:57 +0200

    feat: add project list component

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,37 @@
+<script>
+// importo axios
+import axios from "axios";
+// importo lo store
+import { api, store } from "../store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  created() {
+    // chiamata axios al b
+    axios.get(api.url + "projects").then((response) => {
+      // carico i dati nello store
+      store.projects = response.data.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <div class="container my-4">
+    <!-- stampo lista progetti -->
+    <div v-for="project in store.projects">
+      <ul>
+        <li>{{ project.id }}</li>
+        <li>{{ project.title }}</li>
+        <li>{{ project.description }}</li>
+      </ul>
+      <hr />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
