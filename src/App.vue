55b243d7b9fe5a5bdff3651bbbe2953666b5de88
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-14 15:48:01 +0200

    feat: add axios call  and print list projects

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
       title: "Boolfolio Vue + Vite",
+      projects: [],
     };
+  },
+
+  created() {
+    // chiamata axios al b
+    axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+      this.projects = response.data.data;
+    });
   },
 };
 </script>
@@ -11,6 +21,16 @@ export default {
 <template>
   <div class="container">
     <h1>{{ title }}</h1>
+
+    <!-- stampo lista progetti -->
+    <div v-for="project in projects">
+      <ul>
+        <li>{{ project.id }}</li>
+        <li>{{ project.title }}</li>
+        <li>{{ project.description }}</li>
+      </ul>
+      <hr />
+    </div>
   </div>
 </template>
 
