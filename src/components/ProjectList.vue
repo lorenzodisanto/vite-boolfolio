ad49270a55508b44428cc4c4d2e951ed0f0b28cf
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-14 23:09:45 +0200

    feat: add pagination cards

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -17,12 +17,20 @@ export default {
 
   components: { ProjectCard },
 
+  methods: {
+    // metodo chiamata api
+    fetchProjects(endpoint = api.url + "projects") {
+      axios.get(endpoint).then((response) => {
+        // carico i dati nello store
+        store.projects = response.data.data;
+        // carico i link di paginazione nello store
+        store.pageLinks = response.data.links;
+      });
+    },
+  },
+
   created() {
-    // chiamata axios al b
-    axios.get(api.url + "projects").then((response) => {
-      // carico i dati nello store
-      store.projects = response.data.data;
-    });
+    this.fetchProjects();
   },
 };
 </script>
@@ -30,8 +38,28 @@ export default {
 <template>
   <main>
     <div class="container my-4">
+      <!-- griglia Card -->
       <div class="row row-cols-3 g-3">
         <project-card v-for="project in store.projects" :project="project" />
+      </div>
+
+      <!-- link paginazione recupero i link dallo store e li ciclo per stamparli-->
+      <div class="mt-4">
+        <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-end">
+            <li
+              v-for="link in store.pageLinks"
+              @click="fetchProjects(link.url)"
+              :class="{
+                active: link.active,
+                disabled: !link.url,
+              }"
+              class="page-item"
+            >
+              <a class="page-link" href="#" v-html="link.label"></a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </main>
