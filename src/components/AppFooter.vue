198906eb5652b7f281e3b3aea9e607f54eaab13e
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-14 17:18:02 +0200

    feat: add header component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  // ricevo l'autore dall'App via props
+  props: { author: String },
+};
+</script>
+
+<template>
+  <footer>
+    <nav class="navbar text-bg-dark">
+      <div class="container">
+        <a
+          class="navbar-brand text-white"
+          href="https://github.com/lorenzodisanto"
+          target="_blank"
+        >
+          &copy; {{ author }}
+        </a>
+      </div>
+    </nav>
+  </footer>
+</template>
+
+<style lang="scss" scoped></style>
