1d8a2b6bd08a64528d7417f61cf2d8c8f9bc1ca7
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-15 16:10:17 +0200

    feat: add Home and Blog pages

---
+++ src/pages/BlogPage.vue
@@ -0,0 +1,18 @@
+<script>
+import ProjectList from "../components/ProjectList.vue";
+export default {
+  data() {
+    return {
+      title: "Boolfolio Home Page",
+    };
+  },
+
+  components: { ProjectList },
+};
+</script>
+
+<template>
+  <project-list></project-list>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      title: "Boolfolio Home Page",
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="wrapper container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
