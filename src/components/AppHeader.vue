bbfd908346ec2fd1abbd07c88feebcd2150a5169
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-15 16:09:50 +0200

    feat: add router link

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,9 +7,34 @@ export default {
 
 <template>
   <header>
-    <nav class="navbar text-bg-dark">
+    <nav class="navbar navbar-expand-lg text-bg-dark">
       <div class="container">
-        <a class="navbar-brand text-white fs-3" href="#"> {{ title }} </a>
+        <a class="navbar-brand text-white fs-3" href="#">{{ title }}</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link :to="{ name: 'home' }" class="nav-link text-white">
+                Home
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link :to="{ name: 'blog' }" class="nav-link text-white">
+                Blog
+              </router-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   </header>
