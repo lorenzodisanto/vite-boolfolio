13b3c74bf3d8902e528dc76136a2028766aa71cc
lorenzodisanto <lore.disanto95@gmail.com>
2024-04-18 18:24:14 +0200

    feat: import bootstrap js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import App from "./App.vue";
 
 import { router } from "./router";
 
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
 const app = createApp(App);
 
 app.use(router);
